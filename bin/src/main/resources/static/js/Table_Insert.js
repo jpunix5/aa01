1583675ae22625d29025e11f2b89ec6e24476ee3
jpunix5 <80165401+jpunix5@users.noreply.github.com>
2021-05-06 18:07:59 +0900

    tset _ obs

---
+++ bin/src/main/resources/static/js/Table_Insert.js
@@ -0,0 +1,32 @@
+//row insert
+function userCreate(tableName) {
+//	var user = $("#Insert_Form").serialize();
+	var user = $("#Insert_Form").serializeObject();
+//	var user = $("#Insert_Form").serializeArray();
+	
+//	console.log("111===============");
+//	console.log(typeof user);
+//	console.log("===============");
+//	console.log(user);
+//	console.log("111===============");
+	
+	if('test_users' == tableName){
+		urladd = "/ajaxwrite.do";
+	}else if('test_board' == tableName){
+		urladd = "/ajaxwriteboard.do";
+	};
+	
+	$.ajax({
+		type: "GET",
+//		url: "/ajaxInsert",
+		url: urladd,
+		data : user,
+		success: function () {
+			
+		}
+//		, error : function(request,status,error){
+//			alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+//		}
+		
+	});
+};
