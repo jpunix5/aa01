04b7876819e1dd9c2d00a0417143bd37a42f9a0c
prg <prg@192.168.219.100>
2021-06-08 23:22:18 +0900

    수정,삭제,세이브 시 페이지 리로드 수정

--- src/main/resources/static/js/Table_Delete.js
+++ src/main/resources/static/js/Table_Delete.js
@@ -2,19 +2,14 @@
 function deletethis(idx, table) {
 	var targetidx = idx;
 	
-//	if('test_users' == table){
-//		urladd = "/ajaxdelete.do";
-//	}else if('test_board' == table){
-//		urladd = "/ajaxdeleteboard.do";
-//	};
-	
 	$.ajax({		
 		type: "GET",
-//		url : urladd,
 		url : "/deleteTable", 
 		data : "table=" + table + "&" + "idx=" + targetidx,
 		success: function () {
+			
 		}
 		
 	});
+	window.location.reload();
 }
